cbbbab2365f77f182e264487cec24998e9c451e0
shubham <shubhamshakya140.gmail.com>
2021-10-06 02:10:15 +0530

    Final Commit

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/mygrocerylist/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mygrocerylist;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000,\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\u0010\u001a\u00020\u00112\b\u0010\u0012\u001a\u0004\u0018\u00010\u0013H\u0014R \u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0006\u0010\u0007\"\u0004\b\b\u0010\tR\u001a\u0010\n\u001a\u00020\u000bX\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\f\u0010\r\"\u0004\b\u000e\u0010\u000f\u00a8\u0006\u0014"}, d2 = {"Lcom/example/mygrocerylist/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "list", "", "Lcom/example/mygrocerylist/GroceryItems;", "getList", "()Ljava/util/List;", "setList", "(Ljava/util/List;)V", "viewModel", "Lcom/example/mygrocerylist/UI/GroceryViewModel;", "getViewModel", "()Lcom/example/mygrocerylist/UI/GroceryViewModel;", "setViewModel", "(Lcom/example/mygrocerylist/UI/GroceryViewModel;)V", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+public final class MainActivity extends androidx.appcompat.app.AppCompatActivity {
+    public com.example.mygrocerylist.UI.GroceryViewModel viewModel;
+    public java.util.List<com.example.mygrocerylist.GroceryItems> list;
+    private java.util.HashMap _$_findViewCache;
+    
+    public MainActivity() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.mygrocerylist.UI.GroceryViewModel getViewModel() {
+        return null;
+    }
+    
+    public final void setViewModel(@org.jetbrains.annotations.NotNull()
+    com.example.mygrocerylist.UI.GroceryViewModel p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<com.example.mygrocerylist.GroceryItems> getList() {
+        return null;
+    }
+    
+    public final void setList(@org.jetbrains.annotations.NotNull()
+    java.util.List<com.example.mygrocerylist.GroceryItems> p0) {
+    }
+    
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+}
